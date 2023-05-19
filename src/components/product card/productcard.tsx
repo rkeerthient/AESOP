b564605a8d62528a3e9136cf29a37c5e7bf2050c
rkeerthi <sunnykeerthi@gmail.com>
2023-05-19 16:09:27 +0530

    added slug

--- src/components/product card/productcard.tsx
+++ src/components/product card/productcard.tsx
@@ -6,79 +6,83 @@ const ProductCard = (props: CardProps<Product>) => {
   const { result } = props;
   return (
     <div className="flex flex-col">
-      <img
-        src={result.rawData.c_prodImageUrls![0]}
-        alt=""
-        className="w-auto h-72"
-      />
-      <div className="flex flex-col space-y-8 text-center text-sm">
-        <div className="flex flex-col space-y-3">
-          <div className="text-gray-800 font-bold">{result.name}</div>
-          <div className="text-gray-600 font-light">
-            {result.rawData.price?.currencyCode} {result.rawData.price?.value}
+      <a href={result.rawData.slug}>
+        <img
+          src={result.rawData.c_prodImageUrls![0]}
+          alt=""
+          className="w-auto h-72"
+        />
+        <div className="flex flex-col space-y-8 text-center text-sm">
+          <div className="flex flex-col space-y-3">
+            <div className="text-gray-800 font-bold">{result.name}</div>
+            <div className="text-gray-600 font-light">
+              {result.rawData.price?.currencyCode} {result.rawData.price?.value}
+            </div>
           </div>
+          {result.rawData.c_suitedTo && (
+            <>
+              <div className="py-2 flex gap-2 text-left h-8">
+                <div className=" text-gray-800 font-bold">Suited to</div>
+                <div className="flex-1">
+                  {result.rawData.c_suitedTo.toString().replace(",", ", ")}
+                </div>
+              </div>
+              <hr className="border border-gray-300" />
+            </>
+          )}
+          {result.rawData.c_skinFeel && (
+            <>
+              <div className="py-2 flex gap-2 text-left">
+                <div className="text-sm text-gray-800 font-bold">Skin feel</div>
+                <div>
+                  {result.rawData.c_skinFeel.toString().replace(",", ", ")}
+                </div>
+              </div>
+              <hr className="border border-gray-300" />
+            </>
+          )}
+          {result.rawData.c_aroma && (
+            <>
+              <div className="py-2 flex gap-2 text-left">
+                <div className="  text-gray-800 font-bold">Aroma</div>
+                <div>
+                  {result.rawData.c_aroma.toString().replace(",", ", ")}
+                </div>
+              </div>
+              <hr className="border border-gray-300" />
+            </>
+          )}
+          {result.rawData.c_ingredients && (
+            <>
+              <div className="py-2 flex gap-2 text-left">
+                <div className="  text-gray-800 font-bold">Key ingredients</div>
+                <div>
+                  {result.rawData.c_ingredients.toString().replace(",", ", ")}
+                </div>
+              </div>
+              <hr className="border border-gray-300" />
+            </>
+          )}
+          {result.rawData.c_sizes && (
+            <>
+              <div className="py-2 flex gap-2 text-left">
+                <div className="  text-gray-800 font-bold">Sizes</div>
+                <div>
+                  <ul className="flex justify-start space-x-6">
+                    {result.rawData.c_sizes.map((item: any, index: any) => (
+                      <li className="gap-2 flex" key={index}>
+                        <input type="radio" value={item} name="gender" />
+                        <div>{item} mL</div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <hr className="border border-gray-300" />
+            </>
+          )}
         </div>
-        {result.rawData.c_suitedTo && (
-          <>
-            <div className="py-2 flex gap-2 text-left h-8">
-              <div className=" text-gray-800 font-bold">Suited to</div>
-              <div className="flex-1">
-                {result.rawData.c_suitedTo.toString().replace(",", ", ")}
-              </div>
-            </div>
-            <hr className="border border-gray-300" />
-          </>
-        )}
-        {result.rawData.c_skinFeel && (
-          <>
-            <div className="py-2 flex gap-2 text-left">
-              <div className="text-sm text-gray-800 font-bold">Skin feel</div>
-              <div>
-                {result.rawData.c_skinFeel.toString().replace(",", ", ")}
-              </div>
-            </div>
-            <hr className="border border-gray-300" />
-          </>
-        )}
-        {result.rawData.c_aroma && (
-          <>
-            <div className="py-2 flex gap-2 text-left">
-              <div className="  text-gray-800 font-bold">Aroma</div>
-              <div>{result.rawData.c_aroma.toString().replace(",", ", ")}</div>
-            </div>
-            <hr className="border border-gray-300" />
-          </>
-        )}
-        {result.rawData.c_ingredients && (
-          <>
-            <div className="py-2 flex gap-2 text-left">
-              <div className="  text-gray-800 font-bold">Key ingredients</div>
-              <div>
-                {result.rawData.c_ingredients.toString().replace(",", ", ")}
-              </div>
-            </div>
-            <hr className="border border-gray-300" />
-          </>
-        )}
-        {result.rawData.c_sizes && (
-          <>
-            <div className="py-2 flex gap-2 text-left">
-              <div className="  text-gray-800 font-bold">Sizes</div>
-              <div>
-                <ul className="flex justify-start space-x-6">
-                  {result.rawData.c_sizes.map((item: any, index: any) => (
-                    <li className="gap-2 flex" key={index}>
-                      <input type="radio" value={item} name="gender" />
-                      <div>{item} mL</div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            <hr className="border border-gray-300" />
-          </>
-        )}
-      </div>
+      </a>
     </div>
   );
 };
