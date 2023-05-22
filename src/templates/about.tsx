194f37063ca70e0efe4445ff3a7668db97ccb541
rkeerthi <sunnykeerthi@gmail.com>
2023-05-22 18:11:00 +0530

    added index

--- src/templates/about.tsx
+++ src/templates/about.tsx
@@ -14,16 +14,20 @@ import {
   GetHeadConfig,
   HeadConfig,
 } from "@yext/pages";
- 
- export const config: TemplateConfig = {
-   name: "about",
- };
- 
- export const getPath: GetPath<TemplateProps> = ({document}) => {
-  return `about`;
- };
- 
- export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({relativePrefixToRoot, path, document}): HeadConfig => {
+
+export const config: TemplateConfig = {
+  name: "index",
+};
+
+export const getPath: GetPath<TemplateProps> = ({ document }) => {
+  return `index`;
+};
+
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+  relativePrefixToRoot,
+  path,
+  document,
+}): HeadConfig => {
   return {
     title: "About Page",
     charset: "UTF-8",
@@ -40,34 +44,53 @@ import {
 };
 
 const About: Template<TemplateRenderProps> = ({
-    relativePrefixToRoot,
-    path,
-    document,
-    __meta
-  }) => {
+  relativePrefixToRoot,
+  path,
+  document,
+  __meta,
+}) => {
   const { _site } = document;
- 
-   return (
+
+  return (
     <>
-      <PageLayout _site={_site} templateData={{__meta, document}}>
+      <PageLayout _site={_site} templateData={{ __meta, document }}>
         <Banner text="About Page"></Banner>
         <div className="centered-container">
           <div className="section space-y-10 px-10">
             <h1 className="">About Page</h1>
             <div className="space-y-5">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
               <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+                sit amet, consectetur, adipisci velit, sed quia non numquam eius
+                modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+                voluptatem. Ut enim ad minima veniam, quis nostrum
+                exercitationem ullam corporis suscipit laboriosam, nisi ut
+                aliquid ex ea commodi consequatur? Quis autem vel eum iure
+                reprehenderit qui in ea voluptate velit esse quam nihil
+                molestiae consequatur, vel illum qui dolorem eum fugiat quo
+                voluptas nulla pariatur?
               </p>
             </div>
           </div>
         </div>
       </PageLayout>
-
     </>
-   );
- };
- 
- export default About;
+  );
+};
+
+export default About;
