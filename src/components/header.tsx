6f2c19c60da536a25814f420943886c3e5fd72bc
rkeerthi <sunnykeerthi@gmail.com>
2023-05-22 20:02:54 +0530

    added back auto complete

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -95,8 +95,20 @@ export default function Header({ _site, verticalKey }: any) {
           </div>
           <div className="ml-10 space-x-4 flex-1">
             <SearchBar
-              customCssClasses={{ searchBarContainer: "!mb-0" }}
-            ></SearchBar>
+              visualAutocompleteConfig={{
+                entityPreviewSearcher: entityPreviewSearcher,
+                includedVerticals: ["products"],
+                renderEntityPreviews: renderEntityPreviews,
+                universalLimit: { products: 4 },
+                entityPreviewsDebouncingTime: 500,
+              }}
+              placeholder="search your product"
+              customCssClasses={{
+                searchBarContainer: "z-50",
+                searchButtonContainer:
+                  "bg-orange-600 rounded-full text-white h-8 w-8",
+              }}
+            />
           </div>
         </div>
         <div className="flex flex-wrap justify-center space-x-6 py-4 md:hidden">
