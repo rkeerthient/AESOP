d14cf40475db6949aed64831a73062fe52c94383
rkeerthi <sunnykeerthi@gmail.com>
2023-05-23 00:19:26 +0530

    fixed facets

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -62,7 +62,10 @@ const SearchResults = ({ verticalKey, cardType, resultsCss }: Props) => {
     <div className="max-w-7xl mx-auto mt-4">
       {verticalKey != "" ? (
         <div className="flex">
-          <div className="w-72 shrink-0 px-5 bg-white pt-5 mr-4">
+          <div
+            className=" shrink-0 px-5  pt-5 mr-4"
+            style={{ maxWidth: "18rem" }}
+          >
             <StandardFacets />
           </div>
           <div className="flex-grow">
